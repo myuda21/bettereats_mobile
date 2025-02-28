9c723e2b6923460ef78aea52e96eb861a4c9687e
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-02-28 21:10:38 +0700

    feat: setup calculator screen

---
+++ lib/module/calculator/calculator_screen.dart
@@ -0,0 +1,14 @@
+import 'package:bettereats_mobile/module/calculator/calculator_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+
+class CalculatorScreen extends GetView<CalculatorController> {
+  const CalculatorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
