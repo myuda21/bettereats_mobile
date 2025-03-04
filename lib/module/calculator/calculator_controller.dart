0c77f49d35c5aa0f32b7f949b4bf0e4e746ccb76
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-03-04 21:41:46 +0700

    feat: add BMI calculator screen

--- lib/module/calculator/calculator_controller.dart
+++ lib/module/calculator/calculator_controller.dart
@@ -2,5 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CalculatorController extends GetxController {
+  var height = ''.obs;
+  var weight = ''.obs;
+  var age = ''.obs;
+  var gender = ''.obs;
 
+  void calculateBMI() {
+    double h = double.tryParse(height.value) ?? 0;
+    double w = double.tryParse(weight.value) ?? 0;
+
+    if (h > 0 && w > 0) {
+      double bmi = w / ((h / 100) * (h / 100));
+      print("BMI: ${bmi.toStringAsFixed(2)}");
+    } else {
+      print("Please enter valid height and weight");
+    }
+  }
 }
